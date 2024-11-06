d7f31e10a659aa4574de6bb22e05527359beea34
cowsmit <40091827+cowsmit@users.noreply.github.com>
2024-11-06 12:10:58 +0700

    [editted] cloud ID

--- main_pipeline.py
+++ main_pipeline.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Specify your BigQuery project ID and dataset.table name
 table_spec = (
-    'stately-gist-435602-u9:'
+    'aerobic-rampart-437502-f0:'
     'aekanun_workshop2.aekanun_dfsqltable_sales'
 )
 
@@ -21,7 +21,7 @@ schema = (
 
 # List of pipeline arguments; Adjust with your Google Cloud settings
 pipeline_args = [
-    '--project=stately-gist-435602-u9',  # Change to your GCP project ID
+    '--project=aerobic-rampart-437502-f0',  # Change to your GCP project ID
     '--runner=DataflowRunner',
     '--region=us-central1',  # Adjust as per your GCP region
     '--staging_location=gs://aekanun_workshop2/temp/staging/',  # Change to your bucket path
@@ -35,7 +35,7 @@ p = beam.Pipeline(options=pipeline_options)
 
 (p 
  | 'Read from PubSub' >> beam.io.ReadFromPubSub(
-     topic="projects/stately-gist-435602-u9/topics/aekanun-transactions"  # Change to your PubSub topic
+     topic="projects/aerobic-rampart-437502-f0/topics/aekanun-transactions"  # Change to your PubSub topic
  )
  | 'Cleanse Data' >> beam.Map(cleanse_data)  # Referencing the cleansing function
  | 'Write to BigQuery' >> beam.io.WriteToBigQuery(
